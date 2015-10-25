08f792c23831533eeeb6331c86a079c31ef04f64
Marco Gario <marco.gario@gmail.com>
2015-10-25 14:26:23 +0100

    Issue 152: Most General substitution
    Made Substituter a super-class for:
     - MGSSubstituter (Most General)
     - MSSSubstituter (Most Specific)
    
    This makes it possible to restore the previous behavior, by changing
    which one is instantiated by the environment.
    
    A warning has been added in the substitute method.
    
    This is a transitional commit. The default is still the old one (MSS)

--- pysmt/substituter.py
+++ pysmt/substituter.py
@@ -15,25 +15,92 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #
+import warnings
+
 from six import iteritems
 
 import pysmt.walkers
 import pysmt.operators as op
 
-class Substituter(pysmt.walkers.DagWalker):
 
+class Substituter(pysmt.walkers.DagWalker):
+    """Performs substitution of a set of terms within a formula.
+
+    Let f be a formula ans subs be a map from formula to formula.
+    Substitution returns a formula f' in which all occurrences of the
+    keys of the map have been replaced by their value.
+
+    There are a few considerations to take into account:
+      - In which order to apply the substitution
+      - How to deal with quantified subformulas
+
+    The order in which we apply the substitutions gives raise to two
+    different approaches: Most General Substitution and Most Specific
+    Substitution. Lets consider the example:
+
+      f = (a & b)
+      subs = {a -> c, (c & b) -> d, (a & b) -> c}
+
+    With the Most General Substitution (MGS) we obtain:
+      f' = c
+    with the Most Specific Substitution (MSS) we obtain:
+      f' = d
+
+    The default behavior before version 0.5 was MSS. However, this
+    leads to unexpected results when dealing with literals, i.e.,
+    substitutions in which both x and Not(x) appear, do not work as
+    expected.  In case of doubt, it is recommended to issue two
+    separate calls to the substitution procedure.
+
+    Quantifiers: ???
+
+    """
     def __init__(self, env):
         pysmt.walkers.DagWalker.__init__(self, env=env, invalidate_memoization=True)
         self.manager = self.env.formula_manager
+        self.orig_subs = None
 
-        self.set_function(self.walk_identity, op.SYMBOL, op.REAL_CONSTANT,
-                          op.INT_CONSTANT, op.BOOL_CONSTANT, op.BV_CONSTANT)
+    def _get_key(self, formula, **kwargs):
+        return formula
 
+    def _push_with_children_to_stack(self, formula, **kwargs):
+        """Add children to the stack."""
+
+        # Deal with quantifiers
+        if formula.is_quantifier():
+            # 1. We create a new substitution in which we remove the
+            #    bound variables from the substitution map
+            substitutions = kwargs["substitutions"]
+            new_subs = {}
+            for k,v in iteritems(substitutions):
+                # If at least one bound variable is in the cone of k,
+                # we do not consider this substitution in the body of
+                # the quantifier.
+                if all(m not in formula.quantifier_vars()
+                       for m in k.get_free_variables()):
+                    new_subs[k] = v
+
+            # 2. We apply the substitution on the quantifier body with
+            #    the new 'reduced' map
+            sub = self.__class__(self.env)
+            res_formula = sub.substitute(formula.arg(0), new_subs)
+
+            # 3. We invoke the relevant function (walk_exists or
+            #    walk_forall) to compute the substitution
+            fun = self.functions[formula.node_type()]
+            res = fun(formula, args=[res_formula], **kwargs)
+
+            # 4. We memoize the result
+            key = self._get_key(formula, **kwargs)
+            self.memoization[key] = res
+        else:
+            pysmt.walkers.DagWalker._push_with_children_to_stack(self,
+                                                                 formula,
+                                                                 **kwargs)
 
     def substitute(self, formula, subs):
-        """Given a map ``subs`` of substitutions, replaces any subformula in
-        formula with the corresponding definition in subs.
-        """
+        """Replaces any subformula in formula with the definition in subs."""
+
         # Check that formula is a term
         if not formula.is_term():
             raise TypeError("substitute() can only be used on terms.")
@@ -57,48 +124,54 @@ class Substituter(pysmt.walkers.DagWalker):
                 raise TypeError(
                     "Value %d does not belong to the Formula Manager." % i)
 
+        self.orig_subs = subs
         return self.walk(formula, substitutions=subs)
 
 
+class MGSubstituter(Substituter):
+    """Performs Most Specific Substitution.
+
+    This is the default behavior sinve version 0.5
+    """
+    def __init__(self, env):
+        Substituter.__init__(self, env=env)
+
+        self.set_function(self.walk_identity_or_replace, *op.all_types())
+        self._inner_idw = pysmt.walkers.IdentityDagWalker(env=self.env)
+
+
+    def walk_identity_or_replace(self, formula, args, **kwargs):
+        """If the formula appears in the substitution, return the substitution.
+           Otherwise, rebuild the formula, with its new children.
+        """
+        if formula in self.orig_subs:
+            return self.orig_subs[formula]
+        else:
+            return self._inner_idw.functions[formula.node_type()](formula, args=args)
+
+# EOC MGSubstituter
+
+
+class MSSubstituter(Substituter):
+    """Performs Most Specific Substitution.
+
+    This was the default beahvior before version 0.5
+    """
+
+    def __init__(self, env):
+        Substituter.__init__(self, env=env)
+        self.set_function(self.walk_identity, op.SYMBOL, op.REAL_CONSTANT,
+                          op.INT_CONSTANT, op.BOOL_CONSTANT, op.BV_CONSTANT)
+
+    def substitute(self, formula, subs):
+        warnings.warn("MSSSubstituter will be deprecated in version 0.5\n"+\
+                      "You should test your code with pysmt.substituter.MGSSubstituter.",
+                      category=DeprecationWarning,
+                      stacklevel=2)
+        return Substituter.substitute(self, formula, subs)
+
     def _get_key(self, formula, **kwargs):
         return formula
-
-
-    def _push_with_children_to_stack(self, formula, **kwargs):
-        """Add children to the stack."""
-
-        # Deal with quantifiers
-        if formula.is_quantifier():
-            # 1. We create a new substitution in which we remove the
-            #    bound variables from the substitution map
-            substitutions = kwargs["substitutions"]
-            new_subs = {}
-            for k,v in iteritems(substitutions):
-                # If at least one bound variable is in the cone of k,
-                # we do not consider this substitution in the body of
-                # the quantifier.
-                if all(m not in formula.quantifier_vars()
-                       for m in k.get_free_variables()):
-                    new_subs[k] = v
-
-            # 2. We apply the substitution on the quantifier body with
-            #    the new 'reduced' map
-            sub = Substituter(self.env)
-            res_formula = sub.substitute(formula.arg(0), new_subs)
-
-            # 3. We invoke the relevant function (walk_exists or
-            #    walk_forall) to compute the substitution
-            fun = self.functions[formula.node_type()]
-            res = fun(formula, args=[res_formula], **kwargs)
-
-            # 4. We memoize the result
-            key = self._get_key(formula, **kwargs)
-            self.memoization[key] = res
-        else:
-            pysmt.walkers.DagWalker._push_with_children_to_stack(self,
-                                                                 formula,
-                                                                 **kwargs)
-
 
     def _substitute(self, formula, subs):
         """Returns the substitution for formula, if one is defined, otherwise
@@ -107,7 +180,6 @@ class Substituter(pysmt.walkers.DagWalker):
         This is an helper function, to simplify the implementation of
         the walk_* functions.
         """
-
         return subs.get(formula, formula)
 
     def walk_and(self, formula, args, substitutions, **kwargs):
@@ -307,4 +379,4 @@ class Substituter(pysmt.walkers.DagWalker):
         return self._substitute(self.manager.BVAShr(args[0], args[1]), substitutions)
 
 
-# EOC Substituter
+# EOC MSSSubstituter
